1e73505a706f4f0c6c2827db8dc79ea2297c7f50
Leonardo Tolosa <tolosa@gmail.com>
2010-10-24 20:53:43 -0300

    Changed movie URLs to load in fixture data

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,16 +28,16 @@ IMDB_SAMPLES = {
   "http://www.imdb.com/title/tt0330508/?fr=c2M9MXxsbT01MDB8ZmI9dXx0dD0xfG14PTIwfGh0bWw9MXxjaD0xfGNvPTF8cG49MHxmdD0xfGt3PTF8cXM9S2FubmV0aGlyZXkgVGhvbmRyaW5hbHxzaXRlPWRmfHE9S2FubmV0aGlyZXkgVGhvbmRyaW5hbHxubT0x;fc=1;ft=1" => "tt0330508",
   "http://www.imdb.com:80/find?q=Matrix+Revolutions;s=tt" => "search_matrix_revolutions",
   "http://www.imdb.com:80/find?q=Star+Trek;s=tt" => "search_star_trek",
-  "http://www.imdb.com:80/title/tt0117731/" => "tt0117731",
-  "http://www.imdb.com:80/title/tt0095016/" => "tt0095016",
-  "http://www.imdb.com:80/title/tt0242653/" => "tt0242653",
+  "http://www.imdb.com:80/title/tt0117731/combined" => "tt0117731",
+  "http://www.imdb.com:80/title/tt0095016/combined" => "tt0095016",
+  "http://www.imdb.com:80/title/tt0242653/combined" => "tt0242653",
   "http://www.imdb.com/title/tt0242653/?fr=c2M9MXxsbT01MDB8ZmI9dXx0dD0xfG14PTIwfGh0bWw9MXxjaD0xfGNvPTF8cG49MHxmdD0xfGt3PTF8cXM9TWF0cml4IFJldm9sdXRpb25zfHNpdGU9ZGZ8cT1NYXRyaXggUmV2b2x1dGlvbnN8bm09MQ__;fc=1;ft=20" => "tt0242653",
   "http://www.imdb.com:80/chart/top" => "top_250",
-  "http://www.imdb.com/title/tt0111161/" => "tt0111161",
-  "http://www.imdb.com/title/tt1352369/" => "tt1352369",
-  "http://www.imdb.com/title/tt0083987/" => "tt0083987",
-  "http://www.imdb.com/title/tt0036855/" => "tt0036855",
-  "http://www.imdb.com/title/tt0110912/" => "tt0110912"
+  "http://www.imdb.com/title/tt0111161/combined" => "tt0111161",
+  "http://www.imdb.com/title/tt1352369/combined" => "tt1352369",
+  "http://www.imdb.com/title/tt0083987/combined" => "tt0083987",
+  "http://www.imdb.com/title/tt0036855/combined" => "tt0036855",
+  "http://www.imdb.com/title/tt0110912/combined" => "tt0110912"
 }
 
 unless ENV['LIVE_TEST']
