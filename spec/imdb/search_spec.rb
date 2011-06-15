74afe9b21d56855579d2e696756dda964d7a6093
Peter Schrammel <peter.schrammel@gmx.de>
2011-06-16 00:42:11 +0200

    fixing spec_helper path

--- spec/imdb/search_spec.rb
+++ spec/imdb/search_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../spec_helper.rb'
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper.rb')
 
 describe "Imdb::Search with multiple search results" do
   
--- spec/imdb/top_250_spec.rb
+++ spec/imdb/top_250_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../spec_helper"
+require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
 describe Imdb::Top250 do
   before(:each) do
