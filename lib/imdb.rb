a1ed3cc215d40291bcb0e4438f76bb7ff01ddfe2
unmanbearpig <unmanbearpig@gmail.com>
2014-04-25 13:31:05 +0400

    Replaced open-uri with net/http to add the ability to customize HTTP headers, and set default language to en-US to get rid of localized titles

--- lib/imdb.rb
+++ lib/imdb.rb
@@ -1,7 +1,7 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require 'open-uri'
+require 'net/http'
 require 'rubygems'
 require 'nokogiri'
 
--- lib/imdb/client.rb
+++ lib/imdb/client.rb
@@ -1,7 +1,30 @@
 module Imdb
   class Client
-    def get(url)
-      open(url)
+    attr_accessor :headers
+
+    def initialize(options = {})
+      @headers = {}
+      @headers['Accept-Language'] = 'en-US,en;q=0.5'
+      @headers.merge! options[:headers] if options.key?(:headers)
+    end
+
+    def get(url_or_uri, redirections_limit = 10)
+      raise ArgumentError, 'Too many redirects' if redirections_limit == 0
+
+      uri = url_or_uri.kind_of?(URI::Generic) ? url_or_uri : URI(url_or_uri)
+
+      response = Net::HTTP.start(uri.hostname, uri.port,
+                      :use_ssl => uri.scheme == 'https') do |http|
+        http.request_get(uri, headers)
+      end
+
+      case response
+      when Net::HTTPRedirection
+        redirect_uri = uri.merge(response['location'])
+        get(redirect_uri, redirections_limit -1)
+      else
+        response.body
+      end
     end
   end
 end
