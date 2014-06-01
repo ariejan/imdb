abdf6fbbb6158ed0339ec86261aa25fd3a8ac12c
Ariejan de Vroom <ariejan@ariejan.net>
2014-06-01 17:09:37 +0200

    Setup easy testing with minitest

--- Rakefile
+++ Rakefile
@@ -4,3 +4,5 @@ require "rake/testtask"
 Rake::TestTask.new(:test) do |t|
   t.libs << "test"
 end
+
+task default: :test
--- test/test_imdb_version.rb
+++ test/test_imdb_version.rb
@@ -1,11 +1,7 @@
 require 'minitest_helper'
 
-class TestImdb < MiniTest::Unit::TestCase
+class TestImdbVersion < MiniTest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Imdb::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
