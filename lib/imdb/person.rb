adb1643705a25ce4412a88333bb9297e3984e356
GuilhermeSad <gorgulhoguilherme@gmail.com>
2014-06-10 18:19:16 -0300

    Additional methods to Person

--- lib/imdb/person.rb
+++ lib/imdb/person.rb
@@ -26,6 +26,31 @@ module Imdb
       end
     end
 
+    # Returns a string containing the birthdate
+    def birthdate
+      document.css('time[itemprop=birthDate]').text.delete("\n").squeeze("\s").strip
+    end
+
+    # Returns an integer containing the age
+    def age
+      Date.today.year - birthdate[/\d+$/].to_i
+    end
+
+    # Returns array of work categories exercised by person, e.g. actor, director, etc.
+    def categories
+      document.css('#jumpto a').map(&:text)
+    end
+
+    # Returns array of movies with person in some category, e.g. as an actor, director, etc.
+    def movies_as(category)
+      movies = document.css("#filmo-head-#{category}").first.next_element.children.to_a
+      movies.reject!{|movie| movie.class == Nokogiri::XML::Text}
+      movies.map do |movie|
+        movie_id = movie.attr("id")[/\d+/]
+        Movie.new(movie_id)
+      end
+    end
+
     private
 
     # Returns a new Nokogiri document for parsing.
@@ -36,6 +61,17 @@ module Imdb
     # Use HTTParty to fetch the raw HTML for this person.
     def self.find_by_id(imdb_id)
       open("http://akas.imdb.com/name/nm#{imdb_id}")
+    end
+
+    # Dynamic aliasing for movies_as method. e.g. movies_as_actor equals movies_as(:actor)
+    def method_missing(method_name, *args)
+      get_movies = method_name.to_s.match(/^movies_as_(.+)$/)
+      category = get_movies[1] if get_movies
+      if get_movies and categories.map(&:downcase).include? category.gsub(/_/," ")
+        movies_as category
+      else
+        super
+      end
     end
 
   end # Person
