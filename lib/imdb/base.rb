1d39ada59104238d24d34070a15104850e4db44e
David Hagege <david.hagege@gmail.com>
2017-03-24 17:33:53 +0900

    Strip the title

--- lib/imdb/base.rb
+++ lib/imdb/base.rb
@@ -143,7 +143,7 @@ module Imdb
       if @title && !force_refresh
         @title
       else
-        @title = document.at("div[@id='tn15title']/h1/text()") rescue nil
+        @title = document.at("div[@id='tn15title']/h1/text()").text.strip rescue nil
       end
     end
 
